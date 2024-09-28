0c48b9277e54bccd9d7f252445452edd9565889e
Lenoliums <elena.borisova0210@yandex.ru>
2024-09-28 17:55:52 +0300

    Request

---
+++ getRequest.js
@@ -0,0 +1,27 @@
+async function getPetById(id) {
+    try {
+      const response = await fetch(`https://petstore3.swagger.io/api/v3/pet/${id}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = (await response.json());
+      console.log('result is: ', JSON.stringify(result));
+      return result;
+    } catch (error) {
+      if (error.message) {
+        console.log('error message: ', error.message);
+      } else {
+        console.log('unexpected error: ', error);
+      }
+    }
+  }
+  
+getPetById('dxc');
+getPetById(1);
